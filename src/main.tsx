4faf43c919b94c73c90f266edd2a5a713a41adec
Cristian Sidor <cristian.sidor@icloud.com>
2025-06-23 14:26:21 +0100

    Added comments

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Link the React component to the DOM element
 createRoot(document.getElementById("root")!).render(<App />);
